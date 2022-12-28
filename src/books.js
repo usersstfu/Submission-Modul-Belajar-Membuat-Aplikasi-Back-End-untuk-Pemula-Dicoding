7edd6ef55c7b908992095851c9259c994ce6674a
Ian <102726911+usersstfu@users.noreply.github.com>
2022-12-28 15:32:11 +0700

    Add files via upload

---
+++ src/books.js
@@ -0,0 +1,4 @@
+/* eslint-disable linebreak-style */
+const books = [];
+
+module.exports = books;
---
+++ src/server.js
@@ -0,0 +1,23 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan dimana yaa, disini dong ${server.info.uri}`);
+};
+
+init();
